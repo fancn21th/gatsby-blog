91794e2f03e064f49f87f225ef04ff9d5b86a63c
范轶洁 <fanyjb@digitalchina.com>
2020-12-19 19:14:55 +0800

    strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e562cdfd9f22065a666d7d8b3436495'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2b0c1046-86b8-4a2e-97f9-b16e291d8cad'
+};
